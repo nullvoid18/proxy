6750deba4bc6299dd49011f4c9118a6f0cc746c8
NullVoid <anthony18anthony@gmail.com>
2020-10-31 04:52:55 +0800

    Adding projects to Github for future use

--- app/src/main/java/com/nullvoid/proxy/MainActivity.java
+++ app/src/main/java/com/nullvoid/proxy/MainActivity.java
@@ -2,13 +2,48 @@ package com.nullvoid.proxy;
 
 import android.app.*;
 import android.os.*;
+import okhttp3.*;
+import java.io.*;
+import android.widget.*;
 
 public class MainActivity extends Activity 
 {
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	private OkHttpClient client;
+	private Request request;
+	private Response response;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		// HTTP Client
+		client = new OkHttpClient();
+		
+		// URL
+		String url = "http://dvd.netflix.com";
+		
+		// Request
+		request= new Request.Builder()
+		.url(url)
+		.build();
+		
+		// Response
+		try
+		{
+			response = client.newCall(request).execute();
+			String result = response.body().toString();
+			showToast(result);
+		}
+		catch (IOException e)
+		{
+			showToast(e.toString());
+		}
+
+	}
+	
+	private void showToast(String message){
+		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+	}
+	
 }
